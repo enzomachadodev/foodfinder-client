24a834afb0619032dc55b832f010ab56063e3a85
Enzo <enzo653fm@gmail.com>
2023-05-26 06:32:36 -0300

    Changed base url for backend deployment

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -5,8 +5,8 @@ export function getAPIClient(ctx?: any) {
 	const { "foodfinder.token": token } = parseCookies(ctx);
 
 	const api = axios.create({
-		baseURL: "http://localhost:3333",
-		//baseURL: "http://18.228.138.40:3333",
+		//baseURL: "http://localhost:3333",
+		baseURL: "http://18.228.138.40:3333",
 	});
 
 	api.interceptors.request.use((config) => {
